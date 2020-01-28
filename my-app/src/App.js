1d7101c05f0222249ebc67ca7e3f42cb0eecb61b
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-01-28 12:57:54 -0800

    Uses hooks to track the state of the text typed in the textarea.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = React.useState('');
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setText(value);
+  };
+  console.log({ text });
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
-      <textarea name='' value='' />
+      <textarea value={text} onChange={handleChange} />
       <h4>Time remaining: 00:00</h4>
-      <h1>Word count: 0</h1>
       <button>Start</button>
+      <h1>Word count: 0</h1>
     </div>
   );
 }
