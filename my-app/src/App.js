91d16a46fc6ff867c724e682defd5be53714d4f8
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-01-28 13:58:50 -0800

    Creates countdown timer.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [text, setText] = React.useState('');
+  const [timeRemaining, setTimeRemaining] = React.useState(5);
 
   const calculateWordCount = text => text ? text.trim().split(" ").length : 0;
 
@@ -11,12 +12,19 @@ function App() {
     setText(value);
   };
 
+  React.useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
   console.log({ text });
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
       <textarea value={text} onChange={handleChange} />
-      <h4>Time remaining: 00:00</h4>
+      <h4>Time remaining: {timeRemaining}</h4>
       <button onClick={() => calculateWordCount(text)}>Start</button>
       <h1>Word count: {calculateWordCount(text)}</h1>
     </div>
