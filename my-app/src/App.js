43198a27bffc9722d9f074f5dbd80d165d02f20a
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-01-28 13:23:36 -0800

    Implements function to calculate the word count in the textarea.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,18 +4,21 @@ import './App.css';
 function App() {
   const [text, setText] = React.useState('');
 
+  const calculateWordCount = text => text ? text.trim().split(" ").length : 0;
+
   const handleChange = (e) => {
     const { value } = e.target;
     setText(value);
   };
+
   console.log({ text });
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
       <textarea value={text} onChange={handleChange} />
       <h4>Time remaining: 00:00</h4>
-      <button>Start</button>
-      <h1>Word count: 0</h1>
+      <button onClick={() => calculateWordCount(text)}>Start</button>
+      <h1>Word count: {calculateWordCount(text)}</h1>
     </div>
   );
 }
