7ffcce59116c7f2932300d205a09c1448aca77a5
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-01-28 12:40:25 -0800

    Creates initial UI.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello?</h1>
+      <h1>How fast do you type?</h1>
+      <textarea name='' value='' />
+      <h4>Time remaining: 00:00</h4>
+      <h1>Word count: 0</h1>
+      <button>Start</button>
     </div>
   );
 }
 
-export default App;
+export { App };
