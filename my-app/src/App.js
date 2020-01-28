4ac38e70c6097b7372033984c7d201d358b91794
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-01-28 14:23:12 -0800

    Makes the start button start the timer.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,28 +4,31 @@ import './App.css';
 function App() {
   const [text, setText] = React.useState('');
   const [timeRemaining, setTimeRemaining] = React.useState(5);
+  const [isTimeRunning, setIsTimeRunning] = React.useState(false);
 
   const calculateWordCount = text => text ? text.trim().split(" ").length : 0;
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     const { value } = e.target;
     setText(value);
   };
+  const handleClick = () => setIsTimeRunning(true);
 
   React.useEffect(() => {
-    if (timeRemaining > 0) {
+    if (timeRemaining > 0 && isTimeRunning) {
       setTimeout(() => {
         setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 1);
       }, 1000);
-    }
-  }, [timeRemaining]);
-  console.log({ text });
+    } else if (!timeRemaining) { setIsTimeRunning(false); }
+    console.log({ isTimeRunning });
+  }, [timeRemaining, isTimeRunning]);
+
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
       <textarea value={text} onChange={handleChange} />
       <h4>Time remaining: {timeRemaining}</h4>
-      <button onClick={() => calculateWordCount(text)}>Start</button>
+      <button onClick={handleClick}>Start</button>
       <h1>Word count: {calculateWordCount(text)}</h1>
     </div>
   );
